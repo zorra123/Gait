23e8ada6dca4279054b10876409fb3c713957fb7
vlad <vlad.sackuta@yandex.ru>
2017-04-05 19:29:17 -0700

    Initial commit

---
+++ Build/CMakeFiles/walking.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/walking.dir/src/Vector.cpp.o"
+  "CMakeFiles/walking.dir/src/Matrix.cpp.o"
+  "CMakeFiles/walking.dir/src/Kinematics.cpp.o"
+  "CMakeFiles/walking.dir/src/Point.cpp.o"
+  "CMakeFiles/walking.dir/Walking.cpp.o"
+  "libwalking.pdb"
+  "libwalking.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/walking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
